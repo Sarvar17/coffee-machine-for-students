998b9480d0728caef8ea1c37f480baa28dabb8fa
Sarvar17 <sshilyasov@edu.hse.ru>
2023-04-27 12:08:27 +0500

    Added CoffeeMaker class

---
+++ src/main/java/com/epam/workshops/CoffeeMaker.java
@@ -0,0 +1,17 @@
+package com.epam.workshops;
+
+public class CoffeeMaker {
+    private String status;
+
+    public CoffeeMaker() {
+
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
